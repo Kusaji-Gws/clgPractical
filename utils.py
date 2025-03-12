02cad95b33befeeee02e013c2986e9c688fc3974
kusaji <gawasomkar7@gmail.com>
2025-03-12 19:45:47 +0530

    added utils.py file

--- utils.py
+++ utils.py
@@ -2,50 +2,69 @@ class TodoList:
     def __init__(self):
         self.tasks = []
 
-    def display_tasks(self):
-        if not self.tasks:
-            print("No tasks to display!")
-        else:
-            print("\nCurrent Tasks:")
-            for i, task in enumerate(self.tasks, 1):
-                print(f"{i}. {task}")
-
     def add_task(self, task):
+        """Add a task to the list."""
         self.tasks.append(task)
-        print(f"Task '{task}' added!")
+        print(f"Task '{task}' added.")
+
+    def view_tasks(self):
+        """Display all tasks in the list."""
+        if not self.tasks:
+            print("No tasks available.")
+        else:
+            print("Your To-Do List:")
+            for index, task in enumerate(self.tasks, 1):
+                print(f"{index}. {task}")
 
     def delete_task(self, task_number):
-        if task_number > 0 and task_number <= len(self.tasks):
-            deleted_task = self.tasks.pop(task_number - 1)
-            print(f"Task '{deleted_task}' deleted!")
+        """Delete a task from the list by its number."""
+        if 0 < task_number <= len(self.tasks):
+            task = self.tasks.pop(task_number - 1)
+            print(f"Task '{task}' deleted.")
         else:
-            print("Invalid task number!")
+            print("Invalid task number.")
+
+    def clear_tasks(self):
+        """Clear all tasks."""
+        self.tasks.clear()
+        print("All tasks cleared.")
+
+def show_menu():
+    """Display the menu options to the user."""
+    print("\n--- To-Do List Menu ---")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Clear All Tasks")
+    print("5. Exit")
+    return input("Choose an option: ")
 
 def main():
-    todo = TodoList()
+    todo_list = TodoList()
 
     while True:
-        print("\nTo-Do List Menu:")
-        print("1. View Tasks")
-        print("2. Add Task")
-        print("3. Delete Task")
-        print("4. Exit")
-        choice = input("Please select an option (1-4): ")
+        choice = show_menu()
 
         if choice == '1':
-            todo.display_tasks()
+            task = input("Enter the task you want to add: ")
+            todo_list.add_task(task)
+
         elif choice == '2':
-            task = input("Enter the task to add: ")
-            todo.add_task(task)
+            todo_list.view_tasks()
+
         elif choice == '3':
-            todo.display_tasks()
-            task_number = int(input("Enter the task number to delete: "))
-            todo.delete_task(task_number)
+            task_number = int(input("Enter task number to delete: "))
+            todo_list.delete_task(task_number)
+
         elif choice == '4':
-            print("Exiting To-Do List. Goodbye!")
+            todo_list.clear_tasks()
+
+        elif choice == '5':
+            print("Exiting the To-Do List App.")
             break
+
         else:
-            print("Invalid choice. Please select a valid option.")
+            print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
